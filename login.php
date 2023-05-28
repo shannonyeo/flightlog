9c22ebe76ec0344ab2767b24f65eee9942c6a573
shannonyeo <134833010+shannonyeo@users.noreply.github.com>
2023-05-29 00:35:51 +0800

    Update login.php

--- login.php
+++ login.php
@@ -2,71 +2,8 @@
 
 session_start();
 
-	include("connection.php");
-	include("function.php");
-
-
-	if($_SERVER['REQUEST_METHOD'] == "POST")
-	{
-		//something was posted
-		$user_name = $_POST['user_name'];
-		$password = $_POST['password'];
-
-		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
-		{
-
-			//read from database
-			$query = "select * from person where user_name = '$user_name' limit 1";
-			$query1 = "select * from person where user_name = 'admin' limit 1";
-			$result = mysqli_query($con, $query);
-			$result1 = mysqli_query($con, $query1);
-
-			if($result)
-			{
-				if($result && mysqli_num_rows($result) > 0)
-				{
-
-					$user_data = mysqli_fetch_assoc($result);
-					
-					if($user_data['password'] === $password)
-					{	
-
-						$_SESSION['user_id'] = $user_data['user_id'];
-						header("Location: index.php");
-					
-				
-					}
-					if($user_data['user_name'] === 'admin')
-					{	
-
-						$_SESSION['user_id'] = $user_data['user_id'];
-						header("Location: admin.php");
-						
-				
-					}
-					
-					
-
-					
-				}
-
-				
-				
-			}
-
-			
-		
-			
-			echo '<script type = "text/javascript"> alert ("Wrong username or password!")</script>';
-		}
-		
-		
-		else
-		{
-			echo '<script type = "text/javascript"> alert ("Wrong username or password!")</script>';
-		}
-		
-	}
+include ("config.php);
+$query = $connect->query("SELECT * FROM fruits");
 
 ?>
 
