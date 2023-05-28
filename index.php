f4b38301eb1586219333f9ecb0d95f4c7edc2e25
shannonyeo <134833010+shannonyeo@users.noreply.github.com>
2023-05-29 00:27:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
+<?php
+include ("config.php);
+$query = $connect->query("SELECT * FROM fruits");
+?>
 
+<table border = "1">
+  <tr>
+    <td>No</td>
+    <td>Fruit name</td>
+   </tr>
+   <?php
+   $no = 1;
+   while($row = $query->fetch_assoc()){
+    echo "<tr>
+         <td>$no</td>
+         <td>{$row['name']}</td>
+         </tr>";
+         $no++
+        }
+?>
+</table>>
